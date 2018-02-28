b86c847c8fe1fb145e13ee72de64dae2007af98a
aditayri <aditayri@gmail.com>
2018-02-28 09:49:57 +0200

    Adding isSplittingSet method

--- src/SplittingSet.java
+++ src/SplittingSet.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
@@ -40,37 +41,73 @@ public class SplittingSet extends Graph<Integer>
 	{
 		Graph<Integer> g =initGraph(DS, num_of_rules);
 		SuperGraph sg = new SuperGraph(g);
-		List<Set<Vertex<Integer>>> allSources = sg.findAllRoots();
+		List<Set<Vertex<Integer>>> allSources = sg.findAllRoots();//each set is a source
 		System.out.println(allSources);
-       /* Comparator<String> comparator = new StringLengthComparator();
-		PriorityQueue<String> pq=new PriorityQueue<String>();
-		pq.add("a");
-		pq.add("ab");
-		pq.add("a");
-		while (pq.size() != 0)
-        {
-            System.out.println(pq.remove());
-        }
-        */
-		SortedSet<Set<Vertex<Integer>>> sor = new TreeSet<>(Comparator.comparing(Set::size));
+    
 		PriorityQueue<Set<Vertex<Integer>>> pq=new PriorityQueue<>(Comparator.comparing(Set::size));
 		for (Set<Vertex<Integer>> set : allSources) 
 		{
 			pq.add(set);
 			
 		}
-		while (pq.size() != 0)
+		/*while (pq.size() != 0)
         {
             System.out.println(pq.remove());
-        }
-		//System.out.println(sor);		
+        }*/
+		List<Set<Vertex<Integer>>> SplittingSet = new ArrayList<>();
+		do
+		{
+			SplittingSet.add(pq.poll());
+			if(isSplittingSet(SplittingSet))
+			{
+				break;
+			}
+
+			
+		}while(true);
+			
 		
+	}
+	
+	/**Check if the list of sets is a splitting set*/
+	public boolean isSplittingSet(List<Set<Vertex<Integer>>> SS)
+	{
+		for (Rule r :DS.T)
+			for (int var : r.head) 
+				if(varInSS(var, SS)&&!ruleInSS(r, SS))
+					return false;				
+		return true;
+	}
+	
+	
+	/**Check if all variable in rule r are in the Splitting Set*/
+	private boolean ruleInSS(Rule r , List<Set<Vertex<Integer>>> SS)
+	{
+		for (int hvar :r.head)//head of the rule 
+			if(!varInSS(hvar,SS))
+				return false;
+		
+		for (int  bvar :r.body)//body of the rule
+			if(!varInSS(bvar,SS))
+				return false;
+		
+		return true;
+	}
+	
+	/**Check if variable is in the Splitting Set*/
+	private boolean varInSS(int var , List<Set<Vertex<Integer>>> SS)
+	{
+		for (Set<Vertex<Integer>> set : SS) 
+			for (Vertex<Integer> vertex : set) 
+				if(vertex.id==var)
+					return true;
+		
+		return false;
 	}
 	
 	
 	public void readfile()
 	{
-
 		Scanner sc;
 		int var ;
 		int index = 0;
